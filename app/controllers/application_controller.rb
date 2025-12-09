2be8765142b5779cfb2f8f954505cd6b17a28aad
Fleta Kurtaj <fletakurtajj@gmail.com>
2025-12-10 00:34:34 +0100

    Fix CSRF issue for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,6 @@
 class ApplicationController < ActionController::API
-  before_action :authorize_request
+  include ActionController::Cookies
 
-  def authorize_request
-    header = request.headers["Authorization"]
-
-    if header.present? && header.starts_with?("Bearer ")
-      token = header.split.last
-      begin
-        decoded = JWT.decode(token, Rails.application.credentials.secret_key_base)
-        @current_user = User.find(decoded[0]["user_id"])
-      rescue JWT::DecodeError
-        render json: { error: "Unauthorized" }, status: :unauthorized
-      end
-    else
-      render json: { error: "Unauthorized" }, status: :unauthorized
-    end
-  end
+  # Disable CSRF because this is an API
+  protect_from_forgery with: :null_session
 end
